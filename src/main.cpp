1f252d4728e9fbbec58103291cf85a0928fefabe
jack gladowsky <jackgladowsky@gmail.com>
2023-02-11 20:14:44 -0500

    read how many triangles in stl file

--- src/main.cpp
+++ src/main.cpp
@@ -33,24 +33,32 @@ struct Facet {
     float v3[3];
 };
 
+unsigned int char_to_uint(char* buf) {
+    return (buf[3] << 24) | (buf[2] << 16) | (buf[1] << 8) | (buf[0]);
+}
 
 int main() {
 
+    char headerArray[80];
+    char nextArray[4];
+    
     // open the binary STL file
-    std::ifstream STL_FILE ("../files/Smooth Sango-Hango.stl");
+    std::ifstream STL_FILE ("../files/Polar Bear STL.stl", std::ios::binary | std::ios::in);
 
     // create .txt file to write to
-    std::ofstream TXT_FILE ("../files/triangles.txt");
+    //std::ofstream TXT_FILE ("../files/triangles.txt");
 
     // read the STL file by character
     if (STL_FILE.is_open()) {
-        char myChar;
 
-        while (STL_FILE) {
-            myChar = STL_FILE.get();
-
-            TXT_FILE << myChar;
-        }
+        STL_FILE.read(headerArray, 80);
+        STL_FILE.read(nextArray, 4);
 
     }
+
+    unsigned int numTriangles = char_to_uint(nextArray);
+
+    std::cout << "Num of Trianges: " << numTriangles << '\n';
+    
+    return 0;
 }
